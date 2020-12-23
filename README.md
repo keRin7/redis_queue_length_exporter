61fc505bb3dca43f84c0a296e0cac4942db55697
Maxim Vorobyev <maxim.vorobyev@app.coolrocket.com>
2020-12-23 15:44:02 +0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # redis_queue_length_exporter
+
+Requires global variables:
+
+* QLIST = "queue1,queue2,queue3,..."
+
+Optional global variables:
+
+* HOSTNAME: (default: 127.0.0.1)
+* PORT: (default: 9000)
+* REDIS_HOSTNAME = (default: 127.0.0.1)
+* REDIS_PORT = (default: 6379)
+* REDIS_PASS = (default: none)
+* ENV = (default: local)
+
+
---
+++ run.py
@@ -0,0 +1,37 @@
+from prometheus_client import make_wsgi_app, Gauge
+from wsgiref.simple_server import make_server
+import redis,sys,os
+
+HOSTNAME = os.getenv('HOSTNAME', '127.0.0.1')
+PORT = os.getenv('PORT', 9000)
+REDIS_HOSTNAME = os.getenv('HOSTNAME', '127.0.0.1')
+REDIS_PORT = os.getenv('PORT', 6379)
+REDIS_PASS = os.getenv('REDIS_PASS', 'none')
+ENV = os.getenv('ENV', 'local')
+QLIST = os.getenv('QLIST').split(',')
+
+if QLIST is None:
+    sys.exit('Please set env: QLIST')
+
+g = Gauge('redis_queue_length', 'Length of queues', ['env','queue_name'])
+
+def get_metrics():
+    r = redis.Redis(host=REDIS_HOSTNAME, port=REDIS_PORT, password=REDIS_PASS)
+    try:
+        r.ping()
+    except redis.ConnectionError:
+        print("Cannot make connection to redis")
+        pass
+    for q in QLIST:
+        qlen = r.llen(q)
+        g.labels(ENV,q).set(qlen)
+
+metrics_app = make_wsgi_app()
+
+def my_app(environ, start_fn):
+    if environ['PATH_INFO'] == '/metrics':
+        get_metrics()
+        return metrics_app(environ, start_fn)
+
+httpd = make_server(HOSTNAME, PORT, my_app)
+httpd.serve_forever()
